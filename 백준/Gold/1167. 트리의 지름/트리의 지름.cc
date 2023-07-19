043c799bfa7c15bcbbaaa88fb9db8925a533f453
ijh1298 <139840247+ijh1298@users.noreply.github.com>
2023-07-19 11:03:00 +0900

    [Gold II] Title: 트리의 지름, Time: 244 ms, Memory: 8528 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,66 @@
+//1967번 트리의 지름 문제에서 입력 부분 수정
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+pair<int, int> mx = { 0, 0 };
+vector<pair<int, int>> t[100001];
+bool visit[100001] = {};
+
+void dfs(int start, int curW) {
+	//최대 거리 갱신
+	if (curW > mx.second) {
+		mx.first = start;
+		mx.second = curW;
+	}
+	
+	for (int i = 0; i < t[start].size(); i++)
+		if (!visit[t[start][i].first]) {
+			visit[t[start][i].first] = true;
+			dfs(t[start][i].first, curW + t[start][i].second);
+		}
+}
+
+int main() {
+	int N;
+	cin >> N;
+
+	int u, v, w;
+	for (int i = 0; i < N; i++) {
+		cin >> u;
+		while (1) {
+			cin >> v;
+			if (v == -1)
+				break;
+			cin >> w;
+			t[u].push_back({ v, w });
+		}
+	}
+
+	/*
+	트리의 지름 찾기
+	1. 임의의 점(1)에서 가장 거리가 먼 vf 찾기
+	2. vf에서 가장 거리가 먼 wf 찾기
+	3. vf와 wf가 트리의 지름
+	*/
+	visit[1] = true;
+	dfs(1, 0);
+
+	pair<int, int> vf;
+	vf.first = mx.first;
+	vf.second = mx.second; //1번 끝
+
+	//mx, visit 초기화
+	mx = { 0, 0 };
+	for (int i = 0; i <= N; i++)
+		visit[i] = false;
+
+	visit[vf.first] = true;
+	dfs(vf.first, 0); //2번 끝
+	//이제 mx는 { vf와 wf간 거리, wf 노드 번호 }를 담고 있음
+
+	cout << mx.second;
+
+	return 0;
+}
