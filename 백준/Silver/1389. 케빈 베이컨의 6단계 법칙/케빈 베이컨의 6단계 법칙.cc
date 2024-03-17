79481b12a2d781acabc4fe4987442dc6a8c88221
ijh1298 <139840247+ijh1298@users.noreply.github.com>
2024-03-17 20:59:22 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.cc
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.cc
@@ -1,53 +1,60 @@
+// 플로이드 워셜 알고리즘
 #include <iostream>
+#include <vector>
+#include <limits.h>
+
+using namespace std;
+int d[101][101] = {};
 
 int main() {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(0);
-	std::cout.tie(0);
-
-	int m[101][101] = {};
-	int msum[101];
-
-	std::fill(m[0], m[100], 999999);
+	ios::sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
 	int N, M;
-	std::cin >> N >> M;
+	cin >> N >> M;
 
-	//자신과의 거리는 0
-	for (int i = 0; i <= N; ++i)
-		m[i][i] = 0;
-
-	for (int i = 0; i < M; ++i) {
-		int u, v;
-		std::cin >> u >> v;
-
-		//양방향
-		m[u][v] = 1;
-		m[v][u] = 1;
-	}
-
-	//플로이드-워셜 알고리즘
-	for (int k = 1; k <= N; ++k)
-		for (int i = 1; i <= N; ++i)
-			for (int j = 1; j <= N; ++j)
-				if (m[i][k] + m[k][j] < m[i][j])
-					m[i][j] = m[i][k] + m[k][j];
-
-	int min = 999999, index = 0;
-
-	//msum[i] == (i번째 유저) ~ (모든 다른 사람들 간의 거리) 총합
-	//플로이드-워셜로 만든 거리 테이블을 합하여 총합 거리가 제일 짧은 사람을 찾기
-	for (int i = 1; i <= N; ++i) {
-		for (int j = 1; j <= N; ++j)
-			msum[i] += m[i][j];
-
-		if (msum[i] < min) {
-			min = msum[i];
-			index = i;
+	// 거리 테이블 초기화
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (i == j)
+				d[i][j] = 0;
+			else
+				d[i][j] = 1e9;
 		}
 	}
 
-	std::cout << index;
+	for (int i = 0; i < M; i++) {
+		int u, v;
+		cin >> u >> v;
+		d[u][v] = 1;
+		d[v][u] = 1;
+	}
+
+	// 플로이드 워셜 구현
+	for (int k = 1; k <= N; k++)
+		for (int i = 1; i <= N; i++)
+			for (int j = 1; j <= N; j++)
+					d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+
+	int cnt[101] = { 0 };
+
+	// 케빈 베이컨 수 구하기
+	// 플로이드 워셜 테이블에서, 다른 노드까지의 합이 제일 작은 인덱스 구하기
+	for (int i = 1; i <= N; i++)
+		for (int j = 1; j <= N; j++)
+			if (d[i][j] != 1e9)
+				cnt[i] += d[i][j];
+
+	int min = 1e9, minId = 1;
+	for (int i = 1; i <= N; i++) {
+		if (cnt[i] < min) {
+			min = cnt[i];
+			minId = i;
+		}
+	}
+
+	cout << minId;
 
 	return 0;
 }
