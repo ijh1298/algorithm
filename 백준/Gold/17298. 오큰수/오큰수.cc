24dacc1fc3e5d51240e74dedf9481c8083dcea84
JunHyuk Im <139840247+ijh1298@users.noreply.github.com>
2024-05-02 15:29:24 +0900

    [Gold IV] Title: 오큰수, Time: 604 ms, Memory: 18352 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int N;
+vector<int> numbers;
+stack<pair<int, int>> stk;
+int NGE[1'000'000] = {};
+
+int main() {
+	cin >> N;
+	int input;
+	for (int i = 0; i < N; i++) {
+		cin >> input;
+		numbers.push_back(input);
+	}
+	for (int i = 0; i < N; i++) {
+		while (!stk.empty() && stk.top().first < numbers[i]) {
+			NGE[stk.top().second] = numbers[i];
+			stk.pop();
+		}
+		stk.push({ numbers[i], i });
+	}
+	for (int i = 0; i < N; i++)
+		if (NGE[i] == 0) NGE[i] = -1;
+
+	for (int i = 0; i < N; i++)
+		cout << NGE[i] << " ";
+
+	return 0;
+}
