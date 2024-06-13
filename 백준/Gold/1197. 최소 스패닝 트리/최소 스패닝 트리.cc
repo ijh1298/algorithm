12a573513500eaf7cde1f1c868bfbac8b794d0ea
JunHyuk Im <139840247+ijh1298@users.noreply.github.com>
2024-06-13 21:07:52 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 68 ms, Memory: 8196 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,82 +1,48 @@
-// Minimum Spanning Tree(MST) 구하는 알고리즘 중 Kruskal Algorithm 사용함
-// 참고 : https://blog.naver.com/ndb796/221230994142
-
+// Prim 알고리즘으로 MST 구하기
 #include <iostream>
+#include <queue>
 #include <vector>
-#include <algorithm>
+using namespace std;
 
-// 부모 노드를 가져 오는 함수
-int getParent(int set[], int x) {
-	if (set[x] == x)
-		return x;
-	return set[x] = getParent(set, set[x]);
-}
-
-// 부모 노드를 병합하는 함수
-void unionParent(int set[], int a, int b) {
-	a = getParent(set, a);
-	b = getParent(set, b);
-	
-	// 더 숫자가 작은 부모로 병합
-	if (a < b)
-		set[b] = a;
-	else
-		set[a] = b;
-}
-
-// 같은 부모를 가지는지 확인
-int find(int set[], int a, int b) {
-	a = getParent(set, a);
-	b = getParent(set, b);
-
-	return a == b ? 1 : 0;
-}
-
-class Edge {
-public:
-	int node[2];
-	int distance;
-	Edge(int a, int b, int distance) {
-		this->node[0] = a;
-		this->node[1] = b;
-		this->distance = distance;
-	}
-	bool operator <(Edge& edge) {
-		return this->distance < edge.distance;
-	}
-};
-
-int set[10001];
+int V, E;
+long long ans = 0;
+vector<pair<int, int>> tree[10001]; // tree[u].push_back({w, v}) : u-v 노드의 가중치 w를 의미
+priority_queue<pair<int, int>> pq;	// pq.push({w, v}) : 현재 MST에서 갈 수 있는 노드 v와 가중치 w
+bool visit[10001] = {};
 
 int main() {
-	int V, E;
-	std::cin >> V >> E;
+	ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+	cin >> V >> E;
 
-	std::vector<Edge> t;
-
-	for (int i = 0; i < E; ++i) {
+	while (E--) {
 		int u, v, w;
-		std::cin >> u >> v >> w;
-		t.push_back(Edge(u, v, w));
+		cin >> u >> v >> w;
+		tree[u].push_back({ -w, v });
+		tree[v].push_back({ -w, u });
 	}
-
-	// 가중치 기준으로 오름차순 정렬
-	std::sort(t.begin(), t.end());
-
-	// 부모 초기화
-	for (int i = 1; i <= V; ++i)
-		set[i] = i;
-
-	int sum = 0;
-	for (int i = 0; i < t.size(); ++i) {
-		// *동일한 부모를 가리키지 않는 경우, 즉 사이클이 발생하지 않을 때만 선택하기*
-		if (!find(set, t[i].node[0], t[i].node[1])) {
-			sum += t[i].distance;
-			unionParent(set, t[i].node[0], t[i].node[1]);
+	// Prim 알고리즘 구현
+	visit[1] = true;
+	// 1번 노드에서 갈 수 있는 모든 노드를 pq에 push
+	while (!tree[1].empty()) {
+		pq.push(tree[1].back());
+		tree[1].pop_back();
+	}
+	while (!pq.empty()) {
+		pair<int, int> next = pq.top();
+		pq.pop();
+		int nextNum = next.second;
+		int nextWeight = -next.first;
+		// 방문했던 노드라면 생략
+		if (visit[nextNum]) 
+			continue;
+		visit[nextNum] = true;
+		ans += nextWeight;
+		// 다음 노드에서 갈 수 있는 모든 노드를 pq에 push
+		while (!tree[nextNum].empty()) {
+			pq.push(tree[nextNum].back());
+			tree[nextNum].pop_back();
 		}
 	}
-
-	std::cout << sum;
-
+	cout << ans;
 	return 0;
 }
