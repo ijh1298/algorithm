483d5cb67787a4bb8b7342f44458e25386abfcf7
JunHyuk Im <imjh0417@gmail.com>
2024-07-01 23:35:22 +0900

    [level 2] Title: 요격 시스템, Time: 15.15 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.kt
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.kt
@@ -6,22 +6,19 @@ class Solution {
         // 오름차순 정렬
         val sortedArray = targets.sortedWith(compareBy<IntArray>{it[0]}.thenBy{it[1]})
         
-        var prevS = sortedArray[0][0]
-        var prevE = sortedArray[0][1]
+        var (prevS, prevE) = sortedArray[0]
         
         for (i in 1 until sortedArray.size) {
-            var curS = sortedArray[i][0]
-            var curE = sortedArray[i][1]
+            val (curS, curE) = sortedArray[i]
             
             if (prevE > curS) { // 범위가 겹치면, 새로운 미사일 쏠 필요가 없음
-                prevS = curS
                 prevE = min(prevE, curE)
             }
             else {              // 새로운 범위면, 새로운 미사일 발사
                 answer++
-                prevS = curS
                 prevE = curE
             }
+            prevS = curS
         }
         return answer
     }
