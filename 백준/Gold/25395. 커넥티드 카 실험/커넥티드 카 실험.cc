5319c46bfb442bb6abe068a57dc0d95cb00537dc
JunHyuk Im <imjh0417@gmail.com>
2026-03-29 15:59:40 +0900

    [Gold III] Title: 커넥티드 카 실험, Time: 272 ms, Memory: 15412 KB -BaekjoonHub

--- 백준/Gold/25395. 커넥티드 카 실험/커넥티드 카 실험.cc
+++ 백준/Gold/25395. 커넥티드 카 실험/커넥티드 카 실험.cc
@@ -4,87 +4,95 @@
 
 using namespace std;
 
-#define MAX 1000000000
+#define MAX 1'000'000'000
+#define MAX_VISIT 1'000'001
 
-int visit_front = MAX, visit_end = 0; // 방문 가능한 pos의 시작과 끝
 int N, S;
-bool visited[1000001];
+int visitable_front = MAX, visitable_end = 0; // 접근 가능한 position의 시작과 끝
 vector<int> positions, fuels;
+bool visitable[MAX_VISIT]; // 연결된 커넥트카 확인
 
-// 현재 연료양에 따라 방문 가능 지점을 갱신
-bool checkVisitable(int curIdx) {
-    if (visited[curIdx])
-        return false;
-        
-    visited[curIdx] = true;
-    int fuel = fuels[curIdx];
-    int pos = positions[curIdx];
+void input();
+void checkVisitable(int curIdx);
+void checkBothSide(int curIdx);
+void output();
 
-    if (pos - fuel < 0) {
-        visit_front = 0;
-    } else {
-        visit_front = min(visit_front, pos - fuel);
-    }
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-    if (pos + fuel > MAX) {
-        visit_end = MAX;
-    } else {
-        visit_end = max(visit_end, pos + fuel);
-    }
-    return true;
+    input();
+    checkVisitable(S - 1);
+    checkBothSide(S - 1);
+    output();
+
+    return 0;
 }
 
-// 투포인터로 확인
+void input() {
+    cin >> N >> S;
+
+    for (int i = 0; i < N; i++) {
+        int position;
+        cin >> position;
+        positions.push_back(position);
+    }
+
+    for (int i = 0; i < N; i++) {
+        int fuel;
+        cin >> fuel;
+        fuels.push_back(fuel);
+    }
+}
+
+// 접근 가능한 positions를 갱신
+void checkVisitable(int curIdx) {
+    visitable[curIdx] = true;
+
+    int curPos = positions[curIdx];
+    int curFuel = fuels[curIdx];
+
+    if (curPos - curFuel > 0) {
+        visitable_front = min(visitable_front, curPos - curFuel);
+    } else {
+        visitable_front = 0;
+    }
+    
+    if (curPos + curFuel < MAX) {
+        visitable_end = max(visitable_end, curPos + curFuel);
+    } else {
+        visitable_end = MAX;
+    }
+}
+
+// 투포인터로 연결되는 커넥티드 카 확인
 void checkBothSide(int curIdx) {
     int leftIdx = curIdx;
     int rightIdx = curIdx;
 
     bool changed = true;
     while (changed) {
+        // 더 이상 연결되지 않으면 종료
         changed = false;
-
-        while (leftIdx > 0 && positions[leftIdx - 1] >= visit_front) {
+        // 왼쪽 차가 접근 가능한 범위인 동안
+        while (leftIdx > 0 && positions[leftIdx - 1] >= visitable_front) {
             leftIdx--;
             checkVisitable(leftIdx);
             changed = true;
         }
 
-        while (rightIdx < N - 1 && positions[rightIdx + 1] <= visit_end) {
+        // 오른쪽 차가 접근 가능한 범위인 동안
+        while (rightIdx < N - 1 && positions[rightIdx + 1] <= visitable_end) {
             rightIdx++;
             checkVisitable(rightIdx);
             changed = true;
         }
-    };
+    }
 }
 
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    
-    // 입력
-    cin >> N >> S;
-    
+void output() {
     for (int i = 0; i < N; i++) {
-        int position;
-        cin >> position;
-        positions.push_back(position);
-    }
-    
-    for (int i = 0; i < N; i++) {
-        int fuel;
-        cin >> fuel;
-        fuels.push_back(fuel);
-    }
-    
-    // DFS
-    checkVisitable(S - 1);
-    checkBothSide(S - 1);
-    
-    // 출력
-    for (int i = 0; i < N; i++) {
-        if (visited[i]) {
+        if (visitable[i]) {
             cout << i + 1 << " ";
         }
     }
-
-    return 0;
 }
